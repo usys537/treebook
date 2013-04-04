eda70d511f2656a9f57fb0bff2e024d73d34c6b8
usman shamaki <usys537@yahoo.com>
2013-04-05 00:11:51 +0100

    Updated profile and added scoped status updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -17,4 +17,8 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
   	assert_response :success
   end
   
+  test "that a profile page works" do
+    get '/usman'
+    assert_response :success
+  end
 end
